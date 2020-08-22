aa14ffd366a824a7fd163b33ef9e193cab88d263
shikhar sharma <shikhar.sharma@gopaisa.in>
2020-08-22 10:23:45 +0530

    setting up mongo DB

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
+
+connectDB();
+
 app.get("/", (req, res) => res.send("Hello fucking world!!!!!"));
+
 const port = process.env.PORT || 8080;
+
 app.listen(port, () => console.log(`The server is running on port ${port}`));
